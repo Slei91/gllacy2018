f706e7f1509769c13243c9d612ca13f247e57755
Slei91 <danilych919191@gmail.com>
2019-02-25 21:54:02 +0300

    доделал разметку + стили
    осталось сделать адаптацию + скрипты подключить

--- js/googleMap.js
+++ js/googleMap.js
@@ -1,14 +1,12 @@
-// initMap() - функция инициализации карты
 function initMap() {
-	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-	var centerLatLng = new google.maps.LatLng(59.938743, 30.323047);
-	// Обязательные опции с которыми будет проинициализированна карта
-	var mapOptions = {
-		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-		zoom: 8               // Зум по умолчанию. Возможные значения от 0 до 21
-	};
-	// Создаем карту внутри элемента #map
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
-// Ждем полной загрузки страницы, после этого запускаем initMap()
-google.maps.event.addDomListener(window, "load", initMap);
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          scrollwheel: false,
+          center: {lat: 59.939222, lng: 30.321901}
+        });
+        var marker = new google.maps.Marker({
+          position: {lat: 59.938722, lng: 30.323801},
+          map: map,
+          icon: "img/map-marker.png"
+        });
+      }
